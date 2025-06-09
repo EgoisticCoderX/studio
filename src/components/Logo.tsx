b48e7ec074d5c0d976d0ed1546055670b7d4d588
Egoistic CoderX <egoisticcoderx@gmail.com>
2025-06-09 07:15:32 +0000

    i can't see

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <Link href="/" className="text-2xl font-bold font-headline text-primary transition-transform duration-200 ease-in-out hover:scale-105">
+    <Link href="/" className="text-2xl font-bold font-headline text-primary transition-transform duration-200 ease-in-out hover:scale-110">
       A.X. Studioz
     </Link>
   );
